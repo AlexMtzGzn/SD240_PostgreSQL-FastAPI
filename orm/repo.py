b1be032208caf95c7604bafeb6e7eb77f0c641f5
AlexMtzGzn <alex13141926@gmail.com>
2024-11-30 19:10:53 -0600

    Generamos Funcion compra_por_id

--- orm/repo.py
+++ orm/repo.py
@@ -7,3 +7,9 @@ from sqlalchemy.orm import Session
 def usuario_por_id(sesion:Session,id_usuario:int):
     print("select * from app.usuarios where id = ", id_usuario)
     return sesion.query(modelos.Usuario).filter(modelos.Usuario.id==id_usuario).first()
+
+#Select * from compras
+#definicion de compra y foto
+def compra_por_id(sesion:Session, id_Compra:int):
+    print("Select * from compra where id= ", id_Compra)
+    return sesion.query(modelos.Compra).filter(modelos.Compra.id==id_Compra).first()
