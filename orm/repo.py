3de1352d64ff7da765c4640a81adb470b425f0ba
AlexMtzGzn <alex13141926@gmail.com>
2024-12-10 13:28:36 -0600

    Lo falntate

--- orm/repo.py
+++ orm/repo.py
@@ -1,7 +1,7 @@
 import orm.modelos as modelos
+import orm.esquemas as esquemas
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
-import orm.esquemas as esquemas
 
 # ------------ Peticiones a usuarios ---------------------
 # Esta función es llamada por api.py
@@ -11,35 +11,111 @@ def usuario_por_id(sesion:Session,id_usuario:int):
     print("select * from app.usuarios where id = ", id_usuario)
     return sesion.query(modelos.Usuario).filter(modelos.Usuario.id==id_usuario).first()
 
-def actualizar_usuario(sesion:Session,id_usuario:int , usr_esquema:esquemas.UsuarioBase):
-    usr_bd = usuario_por_id(sesion,id_usuario)
-    if usr_bd is not None:
-        usr_bd.nombre = usr_esquema.nombre
-        usr_bd.edad = usr_esquema.edad
-        usr_bd.domicilio = usr_esquema.domicilio
-        usr_bd.email = usr_esquema.email
-        usr_bd.password = usr_esquema.password
-        
+# Buscar fotos por id de usuario
+# GET '/usuarios/{id}/fotos'
+# select * from app.fotos where id_usuario=id
+def fotos_por_id_usuario(sesion:Session,id_usuario:int):
+    print("select * from app.fotos where id_usuario=", id_usuario)
+    return sesion.query(modelos.Foto).filter(modelos.Foto.id_usuario==id_usuario).all() 
+
+# select * from app.compras where id_usuario=id
+def compras_por_id_usuario(sesion:Session,id_usuario:int):
+    print("select * from app.compras where id_usuario=", id_usuario)
+    return sesion.query(modelos.Compra).filter(modelos.Compra.id_usuario==id_usuario).all() 
+
+# Borra fotos por id de usuario
+# DELETE '/usuarios/{id}/fotos'
+# delete from app.fotos where id_usuario=id
+def borrar_fotos_por_id_usuario(sesion:Session,id_usuario:int):
+    print("delete from app.fotos where id_usuario=",id_usuario)
+    fotos_usr = fotos_por_id_usuario(sesion, id_usuario)
+    if fotos_usr is not None:
+        for foto_usuario in fotos_usr:
+            sesion.delete(foto_usuario)
         sesion.commit()
-        sesion.refresh(usr_bd)
-        print(usr_bd)
-        return usr_bd
-    else :
-        respuesta = {"mensaje","Usuario no encontrado"}
-        return respuesta
 
-
-#Select * from compras
-#definicion de compra y foto
-def compra_por_id(sesion:Session, id_Compra:int):
-    print("Select * from compra where id= ", id_Compra)
-    return sesion.query(modelos.Compra).filter(modelos.Compra.id==id_Compra).first()
+# Borra compras por id de usuario
+# DELETE '/usuarios/{id}/compras'
+# delete from app.compras where id_usuario=id
+def borrar_compras_por_id_usuario(sesion:Session,id_usuario:int):
+    print("delete from app.compras where id_usuario=",id_usuario)
+    compras_usr = compras_por_id_usuario(sesion, id_usuario)
+    if compras_usr is not None:
+        for compra_usuario in compras_usr:
+            sesion.delete(compra_usuario)
+        sesion.commit()
 
 # GET '/usuarios'
 # select * from app.usuarios
 def devuelve_usuarios(sesion:Session):
     print("select * from app.usuarios")
     return sesion.query(modelos.Usuario).all()
+
+#POST '/usuarios'
+def guardar_usuario(sesion:Session, usr_nuevo:esquemas.UsuarioBase):
+    #1.- Crear un nuevo objeto de la clase modelo Usuario
+    usr_bd = modelos.Usuario()
+    #2.- Llenamos el nuevo objeto con los parámetros que nos paso el usuario
+    usr_bd.nombre = usr_nuevo.nombre
+    usr_bd.edad = usr_nuevo.edad
+    usr_bd.domicilio = usr_nuevo.domicilio
+    usr_bd.email = usr_nuevo.email
+    usr_bd.password = usr_nuevo.password
+    #3.- Insertar el nuevo objeto a la BD
+    sesion.add(usr_bd)
+    #4.- Confirmamos el cambio
+    sesion.commit()
+    #5.- Hacemos un refresh
+    sesion.refresh(usr_bd)
+    return usr_bd
+
+#PUT '/usuarios/{id}'
+# UPDATE app.usuarios
+# SET nombre=usr_esquema.nombre, edad=usr_esquema.edad, 
+# domicilio=usr_esquema.domicilio, email=usr_esquema.email,
+# password=usr_esquema.password
+# WHERE id = id_usuario
+def actualiza_usuario(sesion:Session,id_usuario:int,usr_esquema:esquemas.UsuarioBase):
+    #1.-Verificar que el usuario existe
+    usr_bd = usuario_por_id(sesion,id_usuario)
+    if usr_bd is not None:
+        #2.- Actualizamos los datos del usuaurio en la BD
+        usr_bd.nombre = usr_esquema.nombre
+        usr_bd.edad = usr_esquema.edad
+        usr_bd.domicilio = usr_esquema.domicilio
+        usr_bd.email = usr_esquema.email
+        usr_bd.password = usr_esquema.password
+        #3.-Confirmamos los cambios
+        sesion.commit()
+        #4.-Refrescar la BD
+        sesion.refresh(usr_bd)
+        #5.-Imprimir los datos nuevos
+        print(usr_esquema)
+        return usr_esquema
+    else:
+        respuesta = {"mensaje":"No existe el usuario"}
+        return respuesta
+
+# DELETE '/usuarios/{id}'
+# delete from app.usuarios where id=id_usuario
+def borra_usuario_por_id(sesion:Session,id_usuario:int):
+    print("delete from app.usuarios where id=", id_usuario)
+    #1.- borro compras del usuario
+    borrar_compras_por_id_usuario(sesion, id_usuario)
+    #2.-borro foto del usuario
+    borrar_fotos_por_id_usuario(sesion, id_usuario)
+    #3.- select para ver si existe el usuario a borrar
+    usr = usuario_por_id(sesion, id_usuario)
+    #4.- Borramos
+    if usr is not None:
+        #Borramos usuario
+        sesion.delete(usr)
+        #Confirmar los cambios
+        sesion.commit()
+    respuesta = {
+        "mensaje": "usuario eliminado"
+    }
+    return respuesta
 
 # ------------ Peticiones a fotos ---------------------
 # GET '/fotos/{id}'
